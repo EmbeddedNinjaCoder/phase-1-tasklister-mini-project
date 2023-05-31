7f918a000b6212b98e82bdd16f3dd8f273ba7299
German Santos <santos.german@gmail.com>
2023-05-31 02:56:45 -0700

    Displays task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  console.log("Hello")
 });
+
+function addingEventListeners(){ 
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  //console.log(e)
+  const task = e.target[0].value
+  //console.log(task)
+  displayTask(task)
+}
+
+function displayTask(task){
+  const taskUl = document.getElementById("tasks")
+  //console.log(taskUl)
+  const taskLi = document.createElement("li")
+  const deleteBtn = document.createElement("button")
+  deleteBtn.textContent = "x"
+
+  deleteBtn.addEventListener("click", deleteTask)
+
+  taskLi.textContent = task + " "
+  //console.log(taskLi)
+  taskLi.appendChild(deleteBtn)
+  taskUl.appendChild(taskLi)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
+
+addingEventListeners()
+
+
+
