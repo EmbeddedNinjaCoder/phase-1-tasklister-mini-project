bfc7f626f9a5f406683227ea406041b5f69e489d
German Santos <santos.german@gmail.com>
2023-05-31 04:49:52 -0700

    Adds priority colors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  addingEventListeners()
   console.log("Hello")
 });
 
@@ -13,29 +14,49 @@ function handleFormSubmit(e) {
   //console.log(e)
   const task = e.target[0].value
   //console.log(task)
-  displayTask(task)
+  //console.log(e.target.priority.value)
+  const priorityLevel = parseInt(e.target.priority.value)
+  //console.log(priorityLevel)
+  displayTask(task, priorityLevel)
+  e.target.reset()
 }
 
-function displayTask(task){
+function displayTask(task, priorityLevel){
   const taskUl = document.getElementById("tasks")
   //console.log(taskUl)
-  const taskLi = document.createElement("li")
-  const deleteBtn = document.createElement("button")
-  deleteBtn.textContent = "x"
 
+  const taskLi = document.createElement("li")
+
+  const deleteBtn = document.createElement("button")
+
+  deleteBtn.textContent = "x"
   deleteBtn.addEventListener("click", deleteTask)
 
   taskLi.textContent = task + " "
   //console.log(taskLi)
+  taskLi.style.color = getPriorityColor(priorityLevel)
   taskLi.appendChild(deleteBtn)
+
   taskUl.appendChild(taskLi)
 }
 
 function deleteTask(e) {
+  //console.log(e)
   e.target.parentNode.remove()
 }
 
-addingEventListeners()
+function getPriorityColor(priorityLevel) {
+  //console.log(priorityLevel)
+  if (priorityLevel === 1) {
+    return("red")
+  } else if (priorityLevel === 2) {
+    return("blue") 
+  } else {
+    return("green")
+  }
+}
+
+
 
 
 
